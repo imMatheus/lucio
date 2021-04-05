31d8eeaf7e8416cb2c40f8c73a4c7b1b2ee509ad
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-04-05 23:32:15 +0200

    🌠 adding problems page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Form from './components/Form'
+import HomePage from './components/HomePage'
+import ProblemsPage from './components/problemspage/ProblemsPage'
 import './global.css'
+
 function App() {
     return (
         <div className='App'>
             {/* Lucio to the moon 🚀🌙 */}
-            <Form />
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        <HomePage />
+                    </Route>
+                    <Route exact path='/problems'>
+                        <ProblemsPage />
+                    </Route>
+                    <Route exact path='/form'>
+                        <Form />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     )
 }
---
+++ src/components/problemspage/ProblemsPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ProblemsPage = () => {
+    return (
+        <div className='problems'>
+            <ProblemCard />
+            <ProblemCard />
+            <ProblemCard />
+            <ProblemCard />
+            <ProblemCard />
+            <ProblemCard />
+            <ProblemCard />
+            <ProblemCard />
+            <ProblemCard />
+            <ProblemCard />
+        </div>
+    )
+}
+
+const ProblemCard = () => {
+    return (
+        <div className='problemcard'>
+            <div className='header'>header</div>
+        </div>
+    )
+}
+
+export default ProblemsPage
