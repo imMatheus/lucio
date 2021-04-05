f17b9abcda850862a0228b9de84c0376e681582b
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-04-05 23:01:23 +0200

    📁 added problems folder

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+    return <div>homepage</div>
+}
+
+export default HomePage
---
+++ src/problems/problems.js
@@ -0,0 +1,38 @@
+export const problems = [
+    {
+        problemName: 'begining',
+        difficulty: 'easy',
+        problemDescription: `Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for b[1], b[2])`,
+        inputFormat: `The first line contains number of testcases T. The2*Tsubsequent lines each describe a test case over2lines: The first contains 3space-separated integers, N , A, and B, respectively. The second contains S (the string Greg wishes to build).`,
+        constrains: ['T is a string', 'W is a integer'],
+        sampleCases: [
+            { input: [9, 'lucioo'], output: [34, 2], explanation: 'he can cuz he cool ngl' },
+            { input: ['uga'], output: [4, 52], explanation: 'he can cuz he cool ngl' },
+        ],
+        inputs: ['arr', 'x'],
+    },
+    {
+        problemName: 'sup',
+        difficulty: 'medium',
+        problemDescription: `Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for b[1], b[2])`,
+        inputFormat: `The first line contains number of testcases T. The2*Tsubsequent lines each describe a test case over2lines: The first contains 3space-separated integers, N , A, and B, respectively. The second contains S (the string Greg wishes to build).`,
+        constrains: ['T is a string', 'W is a integer'],
+        sampleCases: [
+            { input: [9, 'lucioo'], output: [34, 2], explanation: 'he can cuz he cool ngl' },
+            { input: ['uga'], output: [4, 52], explanation: 'he can cuz he cool ngl' },
+        ],
+        inputs: ['arr', 'x'],
+    },
+    {
+        problemName: 'hardProblem',
+        difficulty: 'hard',
+        problemDescription: `Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for b[1], b[2])`,
+        inputFormat: `The first line contains number of testcases T. The2*Tsubsequent lines each describe a test case over2lines: The first contains 3space-separated integers, N , A, and B, respectively. The second contains S (the string Greg wishes to build).`,
+        constrains: ['T is a string', 'W is a integer'],
+        sampleCases: [
+            { input: [9, 'lucioo'], output: [34, 2], explanation: 'he can cuz he cool ngl' },
+            { input: ['uga'], output: [4, 52], explanation: 'he can cuz he cool ngl' },
+        ],
+        inputs: ['arr', 'x'],
+    },
+]
