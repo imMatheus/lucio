af41ab534bf77cd83ad6988c7be7ed6e49b064ad
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-04-17 14:30:18 +0200

    removed previus problem and added 4 new, svg and png

--- src/css-problems/cssProblems.js
+++ src/css-problems/cssProblems.js
@@ -1,63 +1,35 @@
-import image1 from './images/problem1.png'
-import image2 from './images/problem2.png'
-import image3 from './images/problem3.png'
-import image4 from './images/problem4.png'
-import image5 from './images/problem5.png'
-import image6 from './images/problem6.png'
-import image7 from './images/problem7.png'
+import image1 from './images/problem1.svg'
+import image2 from './images/problem2.svg'
+import image3 from './images/problem3.svg'
+import image4 from './images/problem3.svg'
+import image5 from './images/problem3.svg'
 import image8 from './images/problem8.png'
-import image9 from './images/problem9.png'
-import image10 from './images/problem10.png'
 
 export const cssProblems = [
     {
         target: 1,
         image: image1,
-        colors: ['#5d3a3a', '#b5e0ba'],
+        colors: ['#160F29', '#246A73'],
     },
     {
         target: 2,
         image: image2,
-        colors: ['#6592CF', '#243D83', '#EEB850'],
+        colors: ['#1c110a', '#e4d6a7'],
     },
     {
         target: 3,
         image: image3,
-        colors: ['#191210', '#ECA03D', '#84271C'],
+        colors: ['#E6C229', '#DF8734', '#D84C3E', '#D11149'],
     },
     {
-        target: 4,
-        image: image4,
-        colors: ['#5C434C', '#F09462', '#F5D6B4'],
+        target: 3,
+        image: image3,
+        colors: ['#E6C229', '#DF8734', '#D84C3E', '#D11149'],
     },
-    {
-        target: 5,
-        image: image5,
-        colors: ['#F2F2B6', '#FF6D00', '#FD4602'],
-    },
-    {
-        target: 6,
-        image: image6,
-        colors: ['#09042A', '#E78481', '#F5BB9C'],
-    },
-    {
-        target: 7,
-        image: image7,
-        colors: ['#998235', '#1A4341', '#F3AC3C'],
-    },
+
     {
         target: 8,
         image: image8,
-        colors: ['#6592CF', '#060F55'],
-    },
-    {
-        target: 9,
-        image: image9,
-        colors: ['#1A4341', '#F3AC3C', '#998235'],
-    },
-    {
-        target: 10,
-        image: image10,
-        colors: ['#293462', '#FE5F55', '#A64942'],
+        colors: ['#14213d', '#fca311'],
     },
 ]
