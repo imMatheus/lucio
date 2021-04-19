e7a957dce40cad829fa3e9c2f5612464706e1abc
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-04-19 22:49:39 +0200

    added 5 new images

--- src/css-problems/cssProblems.js
+++ src/css-problems/cssProblems.js
@@ -2,15 +2,21 @@ import image1 from './images/problem1.svg'
 import image2 from './images/problem2.png'
 import image3 from './images/problem3.png'
 import image4 from './images/problem4.png'
-import image5 from './images/problem5.svg'
-import image6 from './images/problem6.svg'
-import image7 from './images/problem7.svg'
+import image5 from './images/problem5.png'
+import image6 from './images/problem6.png'
+import image7 from './images/problem7.png'
 import image8 from './images/problem8.png'
 import image9 from './images/problem9.png'
 import image10 from './images/problem10.png'
 import image11 from './images/problem11.png'
 import image12 from './images/problem12.png'
 import image13 from './images/problem13.png'
+import image16 from './images/problem16.png'
+import image17 from './images/problem17.png'
+import image18 from './images/problem18.png'
+import image19 from './images/problem19.png'
+import image20 from './images/problem20.png'
+import image21 from './images/problem21.png'
 
 export const cssProblems = [
     {
@@ -78,5 +84,35 @@ export const cssProblems = [
         target: 13,
         image: image13,
         colors: ['#00171f', '#00a8e8', '#007ea7'],
-     },
+    },
+    {
+        target: 16,
+        image: image16,
+        colors: ['#003049', '#d62828', '#f77f00', '#fcbf49', '#eae2b7'],
+    },
+    {
+        target: 17,
+        image: image17,
+        colors: ['#007a33', '#ffffff', '#cb333b'],
+    },
+    {
+        target: 18,
+        image: image18,
+        colors: ['#ffffff', '##d21034', '#005293'],
+    },
+    {
+        target: 19,
+        image: image19,
+        colors: [' #004B87', '#FFCD00'],
+    },
+    {
+        target: 20,
+        image: image20,
+        colors: [' #FFFFFF', '#e4002b', '#004b87'],
+    },
+    {
+        target: 21,
+        image: image21,
+        colors: [' #FFFFFF', '#e4002b'],
+    },
 ]
