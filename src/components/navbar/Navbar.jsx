3c38ac0eca68f2f0f2757453972f57c32bd8d413
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-05-02 22:18:34 +0200

    added leaderboard

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,6 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
     const { currentUser, logout } = useAuth()
     const history = useHistory()
     const [userData, setUserData] = useState()
-
     const fetchUser = async (user) => {
         // getting the users data from firestore
         const response = fs.collection('users').doc(user.uid)
@@ -24,64 +23,7 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
         } else {
             setUserData(null)
         }
-        const userRef = db.ref('css')
-        userRef.on('value', (snapshot) => {
-            let leaderBoard = {}
-            let data = snapshot.val()
-            console.log(data)
-            data?.forEach((problem) => {
-                const submissions = problem.submissions
-                if (submissions) {
-                    for (const uid in submissions) {
-                        if (leaderBoard[uid] > 0) {
-                            leaderBoard[uid] += submissions[uid].score
-                        } else {
-                            leaderBoard[uid] = submissions[uid].score
-                        }
-                    }
-                }
-            })
-            // quickSort algorithm
-            function quickSortBasic(array) {
-                if (array.length < 2) {
-                    return array
-                }
-
-                var pivot = array[0]
-                var lesserArray = []
-                var greaterArray = []
-
-                for (var i = 1; i < array.length; i++) {
-                    if (array[i].score > pivot.score) {
-                        greaterArray.push(array[i])
-                    } else {
-                        lesserArray.push(array[i])
-                    }
-                }
-
-                return quickSortBasic(lesserArray).concat(pivot, quickSortBasic(greaterArray))
-            }
-
-            console.log(leaderBoard)
-            let arr1 = []
-            for (const uid in leaderBoard) {
-                console.log(leaderBoard[uid])
-                // if the user does not have a score we don't push it to leader-board
-                if (leaderBoard[uid]) {
-                    arr1.push({ uid: uid, score: leaderBoard[uid] })
-                }
-            }
-            console.log(arr1)
-
-            // basic implementation, where pivot is the first element
-
-            // quickSortBasic(arr1, 0, arr1.length - 1)
-            arr1 = quickSortBasic(arr1)
-            console.log('Sorted array: ')
-            console.log(arr1)
-        })
-    }, [currentUser])
-
+    }, [])
     const logoutHandler = async () => {
         try {
             await logout()
@@ -120,13 +62,13 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
                 {currentUser && <p>Email: {currentUser.email}</p>}
             </div>
             <div className='navbar-right'>
-                <div
+                {/* <div
                     className={isDarkMode ? 'dark-theme-toogler dark' : 'dark-theme-toogler light'}
                     onClick={toogleThemeHandler}
                 >
                     <NightsStayIcon /> <WbSunnyIcon />
                     <div className='theme-btn'></div>
-                </div>
+                </div> */}
                 {currentUser && (
                     <div className='outline-btn' onClick={logoutHandler}>
                         Log Out
