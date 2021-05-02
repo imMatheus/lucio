6968812718319350ecd713066d15797bb4e43ad9
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-05-03 00:52:13 +0200

    added blob to homescreen

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -25,6 +25,7 @@ const HomePage = () => {
         } else {
             setUserData(null)
         }
+        // loading the leader-board
         const userRef = db.ref('css')
         userRef.on('value', async (snapshot) => {
             let leaderBoardObj = {}
@@ -106,14 +107,19 @@ const HomePage = () => {
     return (
         <div className='homepage'>
             <div className='herobanner'>
-                <h1>LucioCode</h1>
-                <div className='buttons'>
-                    <Link to='/problems'>
-                        <div className='outline-btn'>Algos</div>
-                    </Link>
-                    <Link to='/css/problems'>
-                        <div className='outline-btn'>Css Arena</div>
-                    </Link>
+                <div className='blobs-container'>
+                    <Blob />
+                </div>
+                <div className='content'>
+                    <h1>LucioCode</h1>
+                    <div className='buttons'>
+                        <Link to='/problems'>
+                            <div className='outline-btn'>Algorithms</div>
+                        </Link>
+                        <Link to='/css/problems'>
+                            <div className='outline-btn'>Css Arena</div>
+                        </Link>
+                    </div>
                 </div>
             </div>
             {leaderBoard && (
@@ -153,6 +159,46 @@ const HomePage = () => {
                 </div>
             )}
         </div>
+    )
+}
+
+const Blob = () => {
+    return (
+        <>
+            <div className={`blob b1`}>
+                {/* This SVG is from https://codepen.io/Ali_Farooq_/pen/gKOJqx */}
+                <svg
+                    // xmlns:xlink='http://www.w3.org/1999/xlink'
+                    version='1.1'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 310 350'
+                >
+                    <path d='M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z' />
+                </svg>
+            </div>
+            <div className={`blob b2`}>
+                {/* This SVG is from https://codepen.io/Ali_Farooq_/pen/gKOJqx */}
+                <svg
+                    // xmlns:xlink='http://www.w3.org/1999/xlink'
+                    version='1.1'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 310 350'
+                >
+                    <path d='M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z' />
+                </svg>
+            </div>
+            <div className={`blob b3`}>
+                {/* This SVG is from https://codepen.io/Ali_Farooq_/pen/gKOJqx */}
+                <svg
+                    // xmlns:xlink='http://www.w3.org/1999/xlink'
+                    version='1.1'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 310 350'
+                >
+                    <path d='M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z' />
+                </svg>
+            </div>
+        </>
     )
 }
 
