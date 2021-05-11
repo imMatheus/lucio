54d94c972244e4440b6fedf7cf0251ae60e91bb2
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-05-11 20:12:33 +0200

    Changed back color pallet of css arena editor

--- src/components/css-arena/EditorComponent.jsx
+++ src/components/css-arena/EditorComponent.jsx
@@ -11,51 +11,48 @@ const EditorComponent = ({ language, setter, starterCode }) => {
                 base: 'vs-dark',
                 inherit: true,
                 rules: [
-                    // { token: '', foreground: '569dc6' }, //everything
-                    { token: '', foreground: '9cdcfe' }, //everything
-                    { token: 'string.html', foreground: 'ffff00' },
+                    { token: '', foreground: 'ffffff' }, //everything
                     { token: 'invalid', foreground: '00ff00' },
                     { token: 'emphasis', fontStyle: 'italic' },
                     { token: 'strong', fontStyle: 'bold' },
-                    { token: 'variable', foreground: 'ff0000' },
-                    { token: 'variable.predefined', foreground: 'ff0000' },
-                    { token: 'constant', foreground: 'ff0000' },
-                    { token: 'comment', foreground: '6a9955', fontStyle: 'italic' }, //comment
-                    { token: 'number', foreground: 'b5cea8' }, //number
-                    { token: 'number.hex', foreground: 'b5cea8' },
-                    { token: 'regexp', foreground: 'd16969' }, //rexexp
-                    { token: 'annotation', foreground: 'ff0000' },
-                    { token: 'type', foreground: '4ec9b0' }, // promise och math
-                    { token: 'delimiter', foreground: 'd4d4d4' }, //stuff
-                    { token: 'delimiter.html', foreground: 'd4d4d4' },
-                    { token: 'delimiter.xml', foreground: 'd4d4d4' },
-                    { token: 'tag', foreground: '569cd6' },
-                    { token: 'tag.id.jade', foreground: '569cd6' },
-                    { token: 'tag.class.jade', foreground: '569cd6' },
+                    { token: 'variable', foreground: '00ff00' },
+                    { token: 'variable.predefined', foreground: '00ff00' },
+                    { token: 'constant', foreground: '00ff00' },
+                    { token: 'comment', foreground: '7f848e', fontStyle: 'italic' },
+                    { token: 'number', foreground: 'd19a66' }, //number
+                    { token: 'number.hex', foreground: 'd19a66' },
+                    { token: 'regexp', foreground: '56b6c2' }, //rexexp
+                    { token: 'annotation', foreground: 'ff00ff' },
+                    { token: 'type', foreground: 'E5C07B' }, // promise och math
+                    { token: 'delimiter', foreground: 'abb2bf' }, //stuff
+                    { token: 'delimiter.html', foreground: 'abb2bf' },
+                    { token: 'delimiter.xml', foreground: 'abb2bf' },
+                    { token: 'tag', foreground: 'E06C75' },
+                    { token: 'tag.id.jade', foreground: 'E06C75' },
+                    { token: 'tag.class.jade', foreground: 'E06C75' },
                     { token: 'meta.scss', foreground: 'e7c547' },
-                    { token: 'metatag', foreground: '569cd6' },
+                    { token: 'metatag', foreground: 'ff00ff' },
                     { token: 'metatag.content.html', foreground: 'd19a66' },
-                    { token: 'metatag.html', foreground: '569cd6' },
-                    { token: 'metatag.xml', foreground: '569cd6' },
-                    { token: 'metatag.php', fontStyle: '569cd6' },
-                    { token: 'key', foreground: '569cd6' },
-                    { token: 'string.key.json', foreground: '569cd6' },
-                    { token: 'string.value.json', foreground: '9cdcfe' },
-                    { token: 'attribute.name', foreground: '9cdcfe' }, // css tag
-                    { token: 'attribute.value', foreground: 'ce9178' }, // typ solid i css
-                    { token: 'attribute.value.number', foreground: 'ce9178' },
-                    { token: 'attribute.value.unit', foreground: 'ce9178' },
-                    { token: 'attribute.value.html', foreground: 'ce9178' },
-                    { token: 'attribute.value.xml', foreground: 'ce9178' },
-                    { token: 'string', foreground: 'ce9178' }, //strings
-                    { token: 'string.html', foreground: 'ce9178' },
-                    { token: 'string.sql', foreground: 'ce9178' },
-                    { token: 'string.yaml', foreground: 'ce9178' },
-                    { token: 'keyword', foreground: 'c586c0' },
+                    { token: 'metatag.html', foreground: 'E06C75' },
+                    { token: 'metatag.xml', foreground: '86b300' },
+                    { token: 'metatag.php', fontStyle: 'bold' },
+                    { token: 'key', foreground: '00ff00' },
+                    { token: 'string.key.json', foreground: '41a6d9' },
+                    { token: 'string.value.json', foreground: '86b300' },
+                    { token: 'attribute.name', foreground: 'd19a66' },
+                    { token: 'attribute.value', foreground: '98c379' },
+                    { token: 'attribute.value.number', foreground: '98c379' },
+                    { token: 'attribute.value.unit', foreground: '98c379' },
+                    { token: 'attribute.value.html', foreground: '98c379' },
+                    { token: 'attribute.value.xml', foreground: '98c379' },
+                    { token: 'string', foreground: '98c379' }, //strings
+                    { token: 'string.html', foreground: 'ff0000' },
+                    { token: 'string.sql', foreground: '98c379' },
+                    { token: 'string.yaml', foreground: '98c379' },
+                    { token: 'keyword', foreground: 'C678DD' },
                     { token: 'keyword.json', foreground: 'C678DD' },
-                    { token: 'keyword.flow', foreground: 'ff0000' },
+                    { token: 'keyword.flow', foreground: 'C678DD' },
                     { token: 'keyword.flow.scss', foreground: 'C678DD' },
-                    { token: 'operator', foreground: 'ff0000' },
                     { token: 'operator.scss', foreground: '666666' },
                     { token: 'operator.sql', foreground: '778899' },
                     { token: 'operator.swift', foreground: '666666' },
