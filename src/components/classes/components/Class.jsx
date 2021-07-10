596c314e096fed65491bdaafa34d7098681ac8ad
imMatheus <matheusmendesbarata03@gmail.com>
2021-07-10 19:14:24 +0200

    now listens in real time for class changes

--- src/components/classes/components/Class.jsx
+++ src/components/classes/components/Class.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
+import mj from './mj-crying.jpg'
 import { fs } from '../../../firebase'
 import { useRouteMatch, useHistory } from 'react-router-dom'
 import { useAuth } from '../../../context/AuthContext'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 export default function Class() {
     const { currentUser } = useAuth()
@@ -19,74 +21,71 @@ export default function Class() {
     const [students, setStudents] = useState([])
     const [emptyRoute, setEmptyRoute] = useState(false)
     const { path, url } = useRouteMatch()
-    console.log(classData)
-    console.log(students)
     //TODO https://www.youtube.com/watch?v=HOM47v73yG8
     const classLink = url.split('/')[url.split('/').length - 1] // splits at all '/' then takes the last one witch should be the link
     useEffect(() => {
+        let unsubscribe
         async function getClass() {
             let isOwnerOfClass = false
             let dummyClass = {}
 
             let usersClassesQuery = classesRef.where('classJoinLink', '==', classLink) // query class link
-            await usersClassesQuery.get().then((querySnapshot) => {
+            unsubscribe = usersClassesQuery.onSnapshot(async (doc) => {
                 //find th class where the given classLink exist
-                if (querySnapshot.empty) return setEmptyRoute(true) // means the class join link was not found, it does not exist
-                querySnapshot.forEach((doc) => {
-                    dummyClass = doc.data() // set the data we got to the dummyClass object
+                if (doc.empty) return setEmptyRoute(true) // means the class join link was not found, it does not exist
+                doc.forEach((d) => {
+                    dummyClass = d.data() // set the data we got to the dummyClass object
                     if (dummyClass.ownerUid === currentUser.uid) isOwnerOfClass = true
                 })
+
+                /**
+                 * @return {Array(String)} students - a list of all the students uid
+                 */
+                await classesRef
+                    .doc(dummyClass.classID)
+                    .collection('students')
+                    .onSnapshot(async (snapshot) => {
+                        let students = []
+                        snapshot.forEach((doc) => {
+                            students.push(doc.data())
+                        })
+
+                        const s = students.map((s) => s.studentUid) // s is an array of the students uid's
+                        if (!s.includes(currentUser.uid) && !isOwnerOfClass) {
+                            // if the currents uses uid doesnt exist in the array of students that means the user doesnt go to that class
+                            // then we check if it is th owner, if it isn't the owner it should not have access to the class
+                            setEmptyRoute(true)
+                        }
+
+                        let studentDummyHolder = []
+                        for (const student of students) {
+                            // getting the user from firestore and storing user details in
+                            const response = fs.collection('users').doc(student.studentUid)
+                            const rawData = await response.get()
+                            const data = rawData.data()
+
+                            if (data) {
+                                // pushing all the data we got of the user from firestore
+                                studentDummyHolder.push({
+                                    ...data,
+                                })
+                            }
+                        }
+                        setUserIsOwnerOfClass(isOwnerOfClass)
+                        setClassData({ ...dummyClass, students })
+                        setStudents(studentDummyHolder)
+                    })
             })
-
-            /**
-             * @return {Array(String)} students - a list of all the students uid
-             */
-            let students = await classesRef
-                .doc(dummyClass.classID)
-                .collection('students')
-                .get()
-                .then((querySnapshot) => {
-                    let g = []
-                    querySnapshot.forEach((doc) => {
-                        g.push(doc.data())
-                    })
-                    return g
-                })
-
-            const s = students.map((s) => s.studentUid) // s is an array of the students uid's
-            if (!s.includes(currentUser.uid) && !isOwnerOfClass) {
-                // if the currents uses uid doesnt exist in the array of students that means the user doesnt go to that class
-                // then we check if it is th owner, if it isn't the owner it should not have access to the class
-                setEmptyRoute(true)
-            }
-
-            let studentDummyHolder = []
-            for (const student of students) {
-                // getting the user from firestore and storing user details in
-                const response = fs.collection('users').doc(student.studentUid)
-                const rawData = await response.get()
-                const data = rawData.data()
-
-                if (data) {
-                    // pushing all the data we got of the user from firestore
-                    studentDummyHolder.push({
-                        ...data,
-                    })
-                }
-            }
-            setUserIsOwnerOfClass(isOwnerOfClass)
-            setClassData({ ...dummyClass, students })
-            setStudents(studentDummyHolder)
         }
         getClass()
+
+        return () => unsubscribe()
     }, [])
 
     // delete class handler
     const deleteClassHandler = async () => {
         if (!window.confirm('Are you sure you want to delete this class?')) return
-        console.log(classData.students)
         for (const s of classData.students) {
-            console.log(s)
             await usersRef
                 .doc(s.studentUid) // go to the user in the users collection
                 .collection('classes') // go in to the users classes
@@ -105,13 +104,6 @@ export default function Class() {
                 console.error('Error removing document: ', error)
             })
 
-        // await usersRef
-        //     .doc(currentUser.uid)
-        //     .collection('classes')
-        //     .doc(classData.classID)
-        //     .delete()
-        //     .then(() => console.log('deleted from users classes'))
-        //     .catch(() => console.log('shit hit the fan'))
         history.push(path.replace(/[*]/g, ''))
     }
 
@@ -140,8 +132,49 @@ export default function Class() {
         history.push(path.replace(/[*]/g, ''))
     }
 
+    function StudentCard({ profileImage, name, email, studentUid }) {
+        const removeStudentHandler = async () => {
+            if (!window.confirm(`Are you sure you want to kick ${name}?`)) return // ask user if they are sure they want to leave
+            await classesRef
+                .doc(classData.classID) // go to the class document
+                .collection('students') // go to the students collection of this class
+                .doc(studentUid) // find the user in the collection
+                .delete() // delete it from the collection
+                .then(() => {
+                    console.log('Document successfully deleted!')
+                })
+                .catch((error) => {
+                    console.error('Error removing document: ', error)
+                })
+
+            await usersRef
+                .doc(studentUid) // go to the user in the users collection
+                .collection('classes') // go in to the users classes
+                .doc(classData.classID) // find the current class
+                .delete() // delete the class from collection
+                .then(() => console.log('deleted from users classes'))
+                .catch(() => console.log('shit hit the fan'))
+        }
+        return (
+            <div className='student-card-container'>
+                <div className='img-wrapper'>
+                    <img src={profileImage || mj} alt='' />
+                </div>
+                <div className='name-wrapper'>
+                    <p>{name}</p>
+                    <p className='email'>{email}</p>
+                </div>
+                {userIsOwnerOfClass && (
+                    <div className='delete-wrapper' onClick={removeStudentHandler}>
+                        <DeleteIcon />
+                    </div>
+                )}
+            </div>
+        )
+    }
+
     return (
-        <div>
+        <div className='class-wrapper'>
             {emptyRoute ? (
                 <h3>couldn't find the class</h3>
             ) : (
@@ -152,23 +185,23 @@ export default function Class() {
                     ) : (
                         <button onClick={leaveClassHandler}>Leave class</button>
                     )}
-                    <h3>{url}</h3>
+                    {/* <h3>{url}</h3>
                     <h3>{path}</h3>
-                    <h3>{JSON.stringify(classData)}</h3>
-                    {students?.map((student, index) => {
-                        //TODO change to uuid
-                        return (
-                            <div key={index}>
-                                <h3>{JSON.stringify({ student })}</h3>
-                                <img
-                                    src={student.profileImage}
-                                    alt='profileImg'
-                                    width='300'
-                                    height='300'
+                    <h3>{JSON.stringify(classData)}</h3> */}
+                    <div className='students-wrapper'>
+                        {students?.map((student, index) => {
+                            //TODO change to uuid
+                            return (
+                                <StudentCard
+                                    key={index}
+                                    name={student.displayName}
+                                    studentUid={student.userUID}
+                                    email={student.email}
+                                    profileImage={student.profileImage}
                                 />
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
                 </>
             )}
         </div>
