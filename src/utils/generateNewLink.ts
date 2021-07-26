4af8d88a52d30f7013ec42bd6eb307841ef343ae
imMatheus <matheusmendesbarata03@gmail.com>
2021-07-26 13:50:30 +0200

    fixed generate new link bug

--- src/utils/generateNewLink.ts
+++ src/utils/generateNewLink.ts
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app'
 
 /**
- * @param {FirebaseReference} ref - a ref to a firestore collection
+ * @param {CollectionReference} ref - a ref to a firestore collection
  * @return {string} class link - a random generated string of 6 characters, exempla 'djA1k8'
  */
 
@@ -9,22 +9,24 @@ export const generateNewLink = async (
     ref: firebase.firestore.CollectionReference
 ): Promise<string> => {
     const LINK_LENGTH = 7 // length of link
-    const alphabet: string = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
     console.log(alphabet.length)
 
     const upperCaseAlphabet: string = alphabet // converts it into uppercase
         .split('')
         .map((c) => c.toUpperCase())
         .join('')
-    const numbers: string = '0123456789-_'
-    const characters: string = alphabet + upperCaseAlphabet + numbers // combine all of them into one string
-    console.log(characters.length)
+    const numbers = '0123456789-_'
+    const characters = alphabet + upperCaseAlphabet + numbers // combine all of them into one string
 
     let link = ''
-    let newLink: boolean = false
+    let newLink = false
     let fails = 0
+
     while (!newLink) {
         ++fails
+        link = ''
+
         // if we failed 3 times we increment the link length with +1
         for (let i = 0; i < (fails > 3 ? LINK_LENGTH + 1 : LINK_LENGTH); i++) {
             const n = Math.floor(Math.random() * characters.length)
