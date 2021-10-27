9b0a4b66090ca77dd746c3771f4a3707c02c7be3
Matheus <77362975+imMatheus@users.noreply.github.com>
2021-10-27 15:08:01 +0200

    cleaned up regsiter screen

--- pages/Register.tsx
+++ pages/Register.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement, useEffect, useState } from 'react'
 import styles from '../styles/Registration.module.scss'
 import useTimeout from '@/hooks/useTimeout'
+import { ArrowRight, Eye, EyeOff } from 'react-feather'
+import * as EmailValidator from 'email-validator'
 
 interface Props {}
 
@@ -46,6 +48,47 @@ function TypedText({ delay = 0, children, ...props }: TypedTextProps) {
 }
 
 export default function Register({}: Props): ReactElement {
+    const [showPassword, setShowPassword] = useState(false)
+    const [showPasswordContainer, setShowPasswordContainer] = useState(false)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isValidEmail, setIsValidEmail] = useState(false)
+
+    useEffect(() => {
+        setIsValidEmail(EmailValidator.validate(email))
+    }, [email])
+
+    const modalBg = '#0c162d'
+
+    function Button({ ...props }: React.HTMLAttributes<HTMLButtonElement>) {
+        return (
+            <button
+                {...props}
+                className='bg-transparent border-[1px] rounded-md py-1 px-3 text-sm border-[#627597] text-[#627597]'
+            >
+                Continue
+            </button>
+        )
+    }
+
+    // function InputField() {
+    //     return (
+    //         // <div className={`flex bg-[${modalBg}] items-center mb-3`}>
+    //         <div className={`flex bg-red-500 items-center mb-3`}>
+    //             <ArrowRight className='text-pink-500 mr-2 w-5' />
+    //             <input
+    //                 value={email}
+    //                 onChange={(e) => {
+    //                     e.preventDefault()
+    //                     setEmail(e.target.value)
+    //                 }}
+    //                 className='flex-1 outline-none self-stretch bg-transparent text-white bg-blue-400'
+    //             />
+    //             <Button />
+    //         </div>
+    //     )
+    // }
+
     return (
         <section className='flex justify-center items-center font-mono'>
             <div className={styles.modal}>
@@ -56,8 +99,54 @@ export default function Register({}: Props): ReactElement {
                 <TypedText className='text-[#00cfc8] text-base font-semibold mt-5' delay={2}>
                     Enter your email
                 </TypedText>
+                <div className={`flex items-center mb-5 text-white `}>
+                    <ArrowRight className='mr-2 w-5' />
+                    <input
+                        value={email}
+                        onChange={(e) => {
+                            e.preventDefault()
+                            setEmail(e.target.value)
+                        }}
+                        className='flex-1 outline-none self-stretch bg-transparent text-white'
+                    />
+                    <button
+                        onClick={() => setShowPasswordContainer(true)}
+                        disabled={!isValidEmail && email !== ''}
+                        className='bg-transparent border-[1px] rounded-md py-1 px-3 text-sm border-[#627597] text-[#627597]'
+                    >
+                        Continue
+                    </button>
+                </div>
 
-                <input />
+                {!isValidEmail && email !== '' && (
+                    <p className='p-3 text-[#627597]'>The email is not valid or already in use</p>
+                )}
+
+                {isValidEmail && (
+                    <>
+                        <TypedText
+                            className='text-[#00cfc8] text-base font-semibold mt-5'
+                            delay={0}
+                        >
+                            Enter your email
+                        </TypedText>
+
+                        <div className='flex bg-red-400'>
+                            <ArrowRight className='text-pink-300' />
+                            <input
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className='flex-1 outline-none'
+                                type={showPassword ? 'text' : 'password'}
+                            />
+                            {showPassword ? (
+                                <Eye onClick={() => setShowPassword((c) => !c)} />
+                            ) : (
+                                <EyeOff onClick={() => setShowPassword((c) => !c)} />
+                            )}
+                        </div>
+                    </>
+                )}
             </div>
         </section>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.scss'
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
-            <Navbar />
+            <p>hello world</p>
         </div>
     )
 }
