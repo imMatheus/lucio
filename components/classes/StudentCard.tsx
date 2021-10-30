e6d924038ce3fbba2bff716b30dcf75ffd6a1777
imMatheus <matheusmendesbarata03@gmail.com>
2021-10-30 21:23:01 +0200

    started on table view for students

--- components/classes/StudentCard.tsx
+++ components/classes/StudentCard.tsx
@@ -9,12 +9,22 @@ interface Props {
 
 export default function StudentCard({ image, name }: Props): ReactElement {
     return (
-        <div className={styles.studentCard}>
-            <span className='rounded-full bg-red-400 w-7 h-7 mr-3 overflow-hidden'>
-                <SVG src={image} />
-            </span>
-            <p>{name}</p>
-            studetn card
-        </div>
+        // <div className={styles.studentCard}>
+        //     <span className='rounded-full bg-red-400 w-6 h-6 mr-3 overflow-hidden'>
+        //         <SVG src={image} />
+        //     </span>
+        //     <p className='mr-3'>{name}</p>
+        //     studetn card
+        // </div>
+        <tr className={styles.studentCard}>
+            <td>
+                <span className='rounded-full bg-red-400 w-6 h-6 mr-3 overflow-hidden'>
+                    <SVG src={image} />
+                </span>
+            </td>
+            <td>impedit modi dolor?</td>
+            <td>10000</td>
+            <td>10000</td>
+        </tr>
     )
 }
--- pages/classes/[id].tsx
+++ pages/classes/[id].tsx
@@ -3,6 +3,8 @@ import { GetServerSideProps } from 'next'
 import getClass from '@/firebase/querys/getClass'
 import StudentCard from '@/components/classes/StudentCard'
 import { useAuth } from '@/context/AuthContext'
+import styles from 'styles/Classes.module.scss'
+
 // This gets called on every request
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const id = context.params?.id
@@ -28,30 +30,53 @@ export default function ClassScreen({ classData }: Props): ReactElement {
 
     console.log(classState)
     console.log(currentUser)
-
+    let x: any[] = []
+    x.length = 40
+    x = x.fill('adam')
     return (
         <div>
             im class
-            <StudentCard
-                image={currentUser?.profileImage || ''}
-                name={currentUser?.displayName || 'asam'}
-            />
-            <StudentCard
-                image={currentUser?.profileImage || ''}
-                name={currentUser?.displayName || ''}
-            />
-            <StudentCard
-                image={currentUser?.profileImage || ''}
-                name={currentUser?.displayName || ''}
-            />
-            <StudentCard
-                image={currentUser?.profileImage || ''}
-                name={currentUser?.displayName || ''}
-            />
-            <StudentCard
-                image={currentUser?.profileImage || ''}
-                name={currentUser?.displayName || ''}
-            />
+            <table className={styles.styledTable}>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Points</th>
+                        <th>Random</th>
+                        <th>Random</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* <tr>
+                        <td>Dom</td>
+                        <td>
+                            perferendis? Perferendis debitis exercitationem vero? Fugit libero
+                            voluptas impedit modi dolor?
+                        </td>
+                        <td>10000</td>
+                        <td>10000</td>
+                    </tr>
+                    <tr className={styles.activeRow}>
+                        <td>Melissa</td>
+                        <td>5150</td>
+                        <td>550</td>
+                        <td>550</td>
+                    </tr> */}
+                    {x.map((_, index: number) => (
+                        <StudentCard
+                            key={index}
+                            image={currentUser?.profileImage || ''}
+                            name={currentUser?.displayName || 'asam'}
+                        />
+                    ))}
+                </tbody>
+            </table>
+            {/* {x.map((_, index: number) => (
+                <StudentCard
+                    key={index}
+                    image={currentUser?.profileImage || ''}
+                    name={currentUser?.displayName || 'asam'}
+                />
+            ))} */}
         </div>
     )
 }
