e727077806c555373e5af712f05f589f3b1bbee4
imMatheus <matheusmendesbarata03@gmail.com>
2022-01-02 05:26:59 +0100

    did stuff

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
 
 const MONGODB_URI = process.env.MONGO_URI
-// 'mongodb+srv://dbadmin:5VSEkv03PmTBzqz3@cluster0.5ndr8.mongodb.net/sample_restaurants?retryWrites=true&w=majority'
 
 if (!MONGODB_URI) {
 	throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
