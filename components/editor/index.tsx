16dfdbd9ff0c6ca2fa96b3de257379885383fcd2
imMatheus <matheusmendesbarata03@gmail.com>
2022-02-13 16:18:44 +0100

    added profile page

---
+++ components/editor/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface indexProps {}
+
+const index: React.FC<indexProps> = ({}) => {
+	return <div></div>
+}
+
+export default index
---
+++ pages/profile.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import type { NextPage } from 'next'
+
+const profile: NextPage = () => {
+	return <main className="bg-gray-600 max-w-7xl mx-auto p-4 md:p-8">Profile</main>
+}
+
+export default profile
