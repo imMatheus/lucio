7e7e87736db49747372278ad3aac19878afc380d
imMatheus <matheusmendesbarata03@gmail.com>
2022-02-16 18:33:51 +0100

    bug fix: refreshign a page did not add .dark to body

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -2,18 +2,17 @@ import React from 'react'
 import Navbar from '@/components/navbar'
 import Toast from '@/components/toast'
 import { useModal } from '@/context/ModalContext'
-import MarkdownModal from '@/components/modals/MarkdownModal'
+import useDarkMode from '@/hooks/useDarkMode'
 
 const Layout: React.FC = ({ children }) => {
 	const { showModal, modal } = useModal()
+	const [theme] = useDarkMode()
+
 	return (
 		<div className="min-h-screen bg-gray-100 text-gray-900 accent-theme dark:bg-gray-900 dark:text-gray-100">
-			{/* <section className="min-h-screen flex flex-col accent-theme dark:dark-mode"> */}
-
 			<Toast />
 			<Navbar />
 			<div className="">{children}</div>
-			{/* <div className="prose dark:prose-invert max-w-none">{children}</div> */}
 			{showModal && modal}
 		</div>
 	)
--- components/navbar/NavLink.tsx
+++ components/navbar/NavLink.tsx
@@ -8,7 +8,7 @@ interface NavLinkProps {
 const NavLink: React.FC<NavLinkProps> = ({ href, children }) => {
 	return (
 		<Link href={href} passHref={true}>
-			<a className="cursor-pointer text-sm font-medium text-gray-800/80 transition-colors hover:text-gray-900 dark:text-gray-200/80 dark:hover:text-gray-50">
+			<a className="cursor-pointer text-sm font-medium text-gray-700 transition-colors hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50">
 				{children}
 			</a>
 		</Link>
