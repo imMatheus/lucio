477aa6c4819649a116b029b03738410d3eef179f
imMatheus <matheusmendesbarata03@gmail.com>
2022-02-21 00:33:05 +0100

    removed stuff

--- pages/css/index.tsx
+++ pages/css/index.tsx
@@ -18,8 +18,6 @@ const index: NextPage = () => {
 				<div className="h-3 w-[3px] rounded-md bg-lime-500 transition-all group-hover:h-3"></div>
 				<div className="h-3 w-[3px] rounded-md bg-lime-500 transition-all group-hover:h-6"></div>
 				<div className="h-5 w-[3px] rounded-md bg-lime-500 transition-all group-hover:h-2"></div>
-				{/* <div className="w-lg"></div>
-				<div className="mt-1.5 ml-1 h-1 w-[3px]2 bg-lime-500"></div> */}
 			</div>
 		</div>
 	)
