f4f7bbf4b2c70c988e9dc14e202a394a31e39241
imMatheus <matheusmendesbarata03@gmail.com>
2022-02-24 10:07:49 +0100

    added loader for code compiler

--- components/classes/classcard/index.tsx
+++ components/classes/classcard/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import ClassType from '@/types/ClassType'
 import { validateThemeColors } from '@/utils/validateClassThemeColors'
 // import { style } from '@dicebear/avatars/dist/utils'
-import { MapPin, Link as LinkIcon, Book, Inbox, Lock } from 'react-feather'
+import { MapPin, Link as LinkIcon, Code, Book, Inbox, Lock } from 'react-feather'
 import Chip from './Chip'
 
 interface ClassesCardProps {
@@ -22,23 +22,22 @@ export default function ClassCard({ data: { _id, name, code, members, theme, pri
 					style={{ backgroundImage: `linear-gradient(to bottom right, ${theme[0]},${theme[1]}` }}
 				>
 					<div
-						className={`min-h-[200px] w-full bg-black/10 p-4 transition hover:bg-black/20 md:py-5 lg:h-80`}
+						className={`flex min-h-[200px] w-full flex-col bg-black/10 p-4 transition hover:bg-black/20 md:py-5 lg:h-80`}
 					>
-						<div className="flex flex-wrap gap-1">
+						<p className="text-two-line lg:text-five-line m-0 mt-2 overflow-hidden text-ellipsis break-words font-serif text-3xl font-black text-white">
+							{name}
+						</p>
+						<p className="font-serif text-sm font-medium">
+							{members.length || 1} {members.length > 1 ? 'members' : 'member'} | 3 tests
+						</p>
+						<div className="mt-auto flex flex-wrap gap-1">
 							<Chip Icon={MapPin}>Harvard</Chip>
-							<Chip>Programing</Chip>
+							<Chip Icon={Code}>Programing</Chip>
 							<Chip Icon={privacy === 'public' ? Book : privacy === 'invite' ? Inbox : Lock}>
 								{privacy}
 							</Chip>
 							<Chip Icon={LinkIcon}>{code}</Chip>
 						</div>
-
-						<p className="text-two-line lg:text-five-line m-0 mt-2 overflow-hidden text-ellipsis break-words pr-0 font-serif text-3xl font-black text-white md:mt-4 md:pr-3 lg:pr-5">
-							{name} hej
-						</p>
-						<p className="font-serif text-sm font-medium lg:text-base">
-							{members.length || 1} {members.length > 1 ? 'members' : 'member'} | 3 tests
-						</p>
 					</div>
 				</div>
 			</a>
---
+++ components/monaco/Loader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './loader.module.scss'
+
+interface LoaderProps {}
+
+const Loader: React.FC<LoaderProps> = ({}) => {
+	return (
+		<section className="flex min-h-[20rem] items-center justify-center bg-gray-100 dark:bg-gray-900">
+			<div className={styles.ball1}></div>
+			<div className={styles.ball2}></div>
+		</section>
+	)
+}
+
+export default Loader
