9e3d5f55e8526cfd26841fd62a3f0c257757c447
imMatheus <matheusmendesbarata03@gmail.com>
2022-04-02 15:31:51 +0200

    now sets valid member fields for created classes

--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -1,7 +1,13 @@
+import { UserJwt } from './user-jwt.interface';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
+export interface IPayload {
+  sub: string;
+  username: string;
+  email: string;
+}
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
@@ -12,8 +18,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: IPayload) {
     // TODO add necessary info for context here
-    return { userId: payload.sub, username: payload.username };
+
+    const userInfo: UserJwt = {
+      userId: payload.sub,
+      username: payload.username,
+      email: payload.email,
+    };
+
+    return userInfo;
   }
 }
--- server/src/classrooms/classrooms.resolver.ts
+++ server/src/classrooms/classrooms.resolver.ts
@@ -5,26 +5,28 @@ import { CreateClassroomInput } from './dto/create-classroom.input';
 import { UpdateClassroomInput } from './dto/update-classroom.input';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-
+import { MyContext } from '@Types/MyContext';
 @Resolver(() => ClassroomType)
 export class ClassroomsResolver {
   constructor(private readonly classroomsService: ClassroomsService) {}
 
   @Mutation(() => ClassroomType)
+  @UseGuards(JwtAuthGuard)
   createClassroom(
     @Args('createClassroomInput') createClassroomInput: CreateClassroomInput,
+    @Context() context: MyContext,
   ) {
-    return this.classroomsService.create(createClassroomInput);
+    const user = context.req.user;
+
+    return this.classroomsService.create(createClassroomInput, user);
   }
 
   @Query(() => [ClassroomType], { name: 'classrooms' })
   @UseGuards(JwtAuthGuard)
-  findAll(@Context() context: any) {
-    console.log('********************************');
+  findAll(@Context() context: MyContext) {
+    const user = context.req.user;
 
-    console.log(context.req.user);
-
-    return this.classroomsService.findAll();
+    return this.classroomsService.findAll(user.userId);
   }
 
   @Query(() => ClassroomType, { name: 'classroom' })
