88977ffabd15ba2d3f6b05ff18d439ec1594be82
imMatheus <matheusmendesbarata03@gmail.com>
2022-04-17 22:12:17 +0200

    added bearer header for apollo request

--- client/context/AuthContext.tsx
+++ client/context/AuthContext.tsx
@@ -8,7 +8,7 @@ import { Data as updateData } from '@/api/auth/update'
 import { useRouter } from 'next/router'
 import { User, LoginInput, CreateUserInput, useLoginMutation, useSignupMutation, useMeQuery } from '@/gql'
 import { client } from '@/apollo'
-import { setCookie } from 'nookies'
+import { setCookie, parseCookies } from 'nookies'
 
 type IUser = User | null
 
@@ -44,13 +44,16 @@ export const AuthProvider: React.FC = ({ children }) => {
 	const [loginMutation, { error: loginError, data: loginData }] = useLoginMutation()
 	const [signupMutation, { error: signupError, data: signupData }] = useSignupMutation()
 
-	// console.log('userData: ', userData)
+	console.log('userData: ', userData)
 	// console.log('loginData: ', loginData)
 	console.log('signupData: ', signupData?.signup)
 	console.log('signupError, ', signupError)
 	console.log('user, ', currentUser)
 
 	useEffect(() => {
+		const { access_token } = parseCookies()
+
+		if (access_token) return
 		setCurrentUser(signupData?.signup.user || null)
 		setCookie(null, 'access_token', signupData?.signup.access_token || '', {
 			maxAge: 30 * 24 * 60 * 60 // 30 days
@@ -70,7 +73,13 @@ export const AuthProvider: React.FC = ({ children }) => {
 	}
 
 	const signup = async (signupInput: CreateUserInput) => {
-		signupMutation({ variables: { signupInput } })
+		console.log('gggg')
+		try {
+			const res = await signupMutation({ variables: { signupInput } })
+			console.log('res: ', res)
+		} catch (error) {
+			console.log(error)
+		}
 
 		// const { redirect_url } = router.query
 
