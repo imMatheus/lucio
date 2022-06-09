fc1061c0ab2d8e2108470762a99799fdf66ca7d3
imMatheus <matheusmendesbarata03@gmail.com>
2022-06-09 16:54:25 +0200

    changed chip color

--- client/components/classes/classcard/Chip.tsx
+++ client/components/classes/classcard/Chip.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Chip: React.FC<Props> = ({ Icon, children }) => {
 	return (
-		<span className="m-0 flex w-max items-center rounded-lg bg-gray-800/30 py-1 px-2 text-xs  font-normal text-clr-text">
+		<span className="m-0 flex w-max items-center rounded-lg bg-gray-800/30 py-1 px-2 text-xs font-normal text-white">
 			{Icon && <Icon className="mr-1 h-3 w-3 flex-shrink-0" />}
 			{children}
 		</span>
