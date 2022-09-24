622817d0eeea2a5ef028bcba503ac90752dc248c
imMatheus <matheusmendesbarata03@gmail.com>
2022-09-24 16:57:08 +0200

    format

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import { env } from "./src/env/server.mjs";
+import { env } from './src/env/server.mjs'
 
 /**
  * Don't be scared of the generics here.
@@ -9,15 +9,15 @@ import { env } from "./src/env/server.mjs";
  * @constraint {{import('next').NextConfig}}
  */
 function defineNextConfig(config) {
-  return config;
+	return config
 }
 
 export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-});
+	reactStrictMode: true,
+	swcMinify: true,
+	// Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en'
+	}
+})
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,17 +1,17 @@
 // src/pages/api/trpc/[trpc].ts
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { env } from "../../../env/server.mjs";
-import { appRouter } from "../../../server/router";
-import { createContext } from "../../../server/router/context";
+import { createNextApiHandler } from '@trpc/server/adapters/next'
+import { env } from '../../../env/server.mjs'
+import { appRouter } from '../../../server/router'
+import { createContext } from '../../../server/router/context'
 
 // export API handler
 export default createNextApiHandler({
-  router: appRouter,
-  createContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(`❌ tRPC failed on ${path}: ${error}`);
-        }
-      : undefined,
-});
+	router: appRouter,
+	createContext,
+	onError:
+		env.NODE_ENV === 'development'
+			? ({ path, error }) => {
+					console.error(`❌ tRPC failed on ${path}: ${error}`)
+			  }
+			: undefined
+})
